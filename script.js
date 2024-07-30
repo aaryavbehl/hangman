1408a87c128a71b96b40df30c44a1ae7a21acf4e
Aaryav Behl <aaryavbehl47@gmail.com>
2024-07-31 00:41:43 +0530

    new file:   index.html

---
+++ script.js
@@ -0,0 +1,64 @@
+const wordElement = document.getElementById("word");
+const wrongLettersElement = document.getElementById("wrong-letters");
+const playAgainButton = document.getElementById("play-button");
+const popup = document.getElementById("popup-container");
+const notification = document.getElementById("notification-container");
+const finalMessage = document.getElementById("final-message");
+const finalMessageRevealWord = document.getElementById(
+  "final-message-reveal-word"
+);
+const figureParts = document.querySelectorAll(".figure-part");
+
+const words = [
+  "application",
+  "programming",
+  "interface",
+  "wizard",
+  "element",
+  "prototype",
+  "callback",
+  "undefined",
+  "arguments",
+  "settings",
+  "selector",
+  "container",
+  "instance",
+  "response",
+  "console",
+  "constructor",
+  "token",
+  "function",
+  "return",
+  "length",
+  "type",
+  "node",
+];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+let playable = true;
+
+const correctLetters = [];
+const wrongLetters = [];
+
+function displayWord() {
+  wordElement.innerHTML = `
+    ${selectedWord
+      .split("") 
+      .map(
+        (letter) => `
+    <span class="letter">
+    ${correctLetters.includes(letter) ? letter : ""}
+    </span>
+    `
+      )
+      .join("")} 
+    `; 
+    
+    const innerWord = wordElement.innerText.replace(/\n/g, "");
+    if (innerWord === selectedWord) {
+      finalMessage.innerText = "Congratulations! You won! 😃";
+      finalMessageRevealWord.innerText = "";
+      popup.style.display = "flex";
+      playable = false;
+    }
+  }
